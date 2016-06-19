c994c5eff8ba30ef8d2964b375b989547a17291d
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-06-18 23:14:47 -0400

    fix API key and styling

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+var API_KEY = 'fec8b5ab27b292a68294261bb21b04a5';
 
 var app = angular.module('my-app', ['ngRoute']);
+
 //route configuration - what route goes to what pages
 app.config(function($routeProvider) {
   $routeProvider
@@ -13,8 +15,7 @@ app.config(function($routeProvider) {
     });
 });
 
-var API_KEY = 'fec8b5ab27b292a68294261bb21b04a5';
-
+//main controller
 app.controller('MainController', function($scope, $http) {
   $http.get('http://api.themoviedb.org/3/movie/now_playing?api_key=' + API_KEY).success(function(movie) {
     $scope.movie = movie;
@@ -27,12 +28,13 @@ app.controller('MainController', function($scope, $http) {
       $scope.movie = movie;
       if (page <= movie.total_pages) {
         page++;
-      };
+      }
     });
   };
 
 });
 
+//details page controller
 app.controller('DetailsController', function($scope, $http, $routeParams) {
   $scope.movieId = $routeParams.movieId;
   $http.get('http://api.themoviedb.org/3/movie/' + $routeParams.movieId + '?api_key=' + API_KEY).success(function(data) {
